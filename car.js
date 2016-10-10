867619ce2792383222dd3054b1f91f79a9ea1c47
Hou Kan Julian Chan <julianchanhk@gmail.com>
2016-10-10 17:30:41 +0800

    mod car.js and script.js

--- car.js
+++ car.js
@@ -2,12 +2,14 @@ function Car(speed, color, convertible) {
   this.speed = 0
   this.color = color
   this.convertible = convertible
-  // this.accelerate = accelerate
-  // this.decelerate = decelerate
-  this.speedUp = function(currentSpeed) {
-    var newSpeed = currentSpeed + accelerate;
-    return ('greenCar was ' + currentSpeed + 'now it is ' + newSpeed)
+  this.accelerate = function (num) {
+    this.speed += num
   }
+  // this.decelerate = decelerate
+  // this.speedUp = function(currentSpeed) {
+  //   var newSpeed = currentSpeed + accelerate;
+  //   return ('greenCar was ' + currentSpeed + 'now it is ' + newSpeed)
+  // }
   this.setColor = function(newColor) {
     this.color = newColor
   }
